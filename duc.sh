4ca29c1ac8f8d926db76c241e01b0ddeee54275f
Benjamin Tamasi <halftome@gmail.com>
2020-05-07 17:49:04 +0100

    silence curl

--- duc.sh
+++ duc.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-system_ip=`curl ${IP4_URL}`
+system_ip=`curl -s ${IP4_URL}`
 echo "System IP: $system_ip"
 
 url="${DO_API_BASE_URL}/domains/${DO_DOMAIN}/records/${DO_RECORD_ID}"
 
 CheckRecord () {
     # Get record IP
-    record_ip=`curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer ${DO_TOKEN}" ${url} | jq -r ".domain_record.data"`
+    record_ip=`curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer ${DO_TOKEN}" ${url} | jq -r ".domain_record.data"`
 
     if [ $record_ip == "null" ]; then
         echo "Error: The record doesn't hold a valid IP!"
@@ -25,7 +25,7 @@ CheckRecord () {
 }
 
 UpdateRecord () {
-    new_ip=`curl -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer ${DO_TOKEN}" -d "{\"data\": \"${system_ip}\"}" ${url} | jq -r ".domain_record.data"`
+    new_ip=`curl -s -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer ${DO_TOKEN}" -d "{\"data\": \"${system_ip}\"}" ${url} | jq -r ".domain_record.data"`
     if [ $new_ip != $system_ip ]; then
         echo "Record update failed"
         exit 3
